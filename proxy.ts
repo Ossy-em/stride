254f9b18713a422c503fdb537b8eedae1ae7977b
Ossy-em <emosinachi@gmail.com>
2026-02-23 22:35:03 +0100

    update

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 // Set this to true to enable maintenance mode
-const MAINTENANCE_MODE = true;
+const MAINTENANCE_MODE = false;
 
 // Your secret password - CHANGE THIS!
 const MAINTENANCE_PASSWORD = 'stride-testing-2024';
